92aca36d5bc2e9c334e7e641244e99f19b3fe238
Myridwan <92353970+MyRidwan@users.noreply.github.com>
2023-03-31 21:22:55 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+REPO="https://raw.githubusercontent.com/myridwan/abc/ipuk/"
+echo -e " [INFO] Downloading Update File"
+sleep 2
+wget -O /tmp/menu-master.zip "${REPO}config/menu.zip" >/dev/null 2>&1
+    mkdir /tmp/menu
+    7z e  /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
+    chmod +x /tmp/menu/*
+    mv /tmp/menu/* /usr/sbin/
+echo -e " [INFO] Update Successfully"
+sleep 2
+exit
