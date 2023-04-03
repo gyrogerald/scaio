d7d02d2625e976cd7bcf357a3faa33037cfa0667
Myridwan <92353970+MyRidwan@users.noreply.github.com>
2023-04-03 08:16:47 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 REPO="https://raw.githubusercontent.com/myridwan/abc/ipuk/"
-echo -e " [INFO] Downloading Update File"
+echo -e " [INFO] Downloading Update File" | lolcat
 sleep 2
 rm -rf /tmp/menu
 wget -O /tmp/menu-master.zip "${REPO}config/menu.zip" >/dev/null 2>&1
@@ -11,7 +11,12 @@ wget -O /tmp/menu-master.zip "${REPO}config/menu.zip" >/dev/null 2>&1
     7z e  /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
     chmod +x /tmp/menu/*
     mv /tmp/menu/* /usr/sbin/
-echo -e " [INFO] Update Successfully"
+echo -e " [INFO] Update Successfully" | lolcat
+mkdir -p /etc/ssh
+mkdir -p /etc/vmx
+mkdir -p /etc/vls
+mkdir -p /etc/tr
+mkdir -p /etc/ss
 rm -rf update.sh
 sleep 2
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
